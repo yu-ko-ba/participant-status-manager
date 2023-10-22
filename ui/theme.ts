0122a11d0b2bcbcd8081227d0c450cc7925882e2
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-10-22 16:05:19 +0900

    Material UIをインストールしました

---
+++ ui/theme.ts
@@ -0,0 +1,8 @@
+import { createTheme } from "@mui/material";
+import "@fontsource-variable/m-plus-1";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: "'M PLUS 1 Variable', sans-serif",
+  },
+});
